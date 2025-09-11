24fe702402daf2d9add0dc6cc17d74fd5268d028
istamw <78308335+istamw@users.noreply.github.com>
2025-09-11 12:32:27 -0300

    intelij

---
+++ Pelicula/src/que.java
@@ -0,0 +1,8 @@
+import uwcse.graphics.GWindow;
+import uwcse.graphics.Oval;
+
+public class que {
+    public static void main(String[] args) {
+        GWindow ventana = new GWindow(1280, 720);
+    }
+}
