5b48e1c8068aad6da17660240d3192e97344b123
Esteban Lopez <78308335+istamw@users.noreply.github.com>
2025-09-22 20:02:36 -0300

    mac

--- Pelicula/src/Main.java
+++ Pelicula/src/Main.java
@@ -11,22 +11,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         GWindow window = new GWindow(800, 600);
-        Cato cato = new Cato(80,200,"imgs/cato/cato.png");
-        Polygon pol = new Polygon(new Color(255,0,68,80),true);
 
-        pol.addPoint(0,0);
-        pol.addPoint(800,0);
-        pol.addPoint(800,600);
-        pol.addPoint(0,600);
 
-        cato.addTo(window);
-        pol.addTo(window);
-        cato.voltearAtras(25);
-        cato.voltearAdelante(25);
-
-        while (true) {
-            pol.rotateAround(400,300,4);
-            Thread.sleep(2);
-        }
     }
 }
